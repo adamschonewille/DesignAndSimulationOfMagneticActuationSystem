fe2549e1e8bae33d998b0a29eb1fee0eca00be35
adamschonewille <adamschonewille@gmail.com>
2019-07-05 10:42:02 -0400

    First Commit. Initializing MATLAB files.

---
+++ DesignAndOptimizationOfNewSystem.m
@@ -0,0 +1,8 @@
+%% Simulations for designing the next novel actuation system.
+%
+%  Author: Adam Schonewille
+%  MASc Student, University of Toronto
+%
+%  Start Date: July 5th 2019
+%  Last Editted: July 5th 2019
+%
---
+++ rotz.m
@@ -0,0 +1,6 @@
+function [Rz] = rotz(angle) 
+
+Rz = [cos(angle), -sin(angle),  0;
+      sin(angle),  cos(angle),  0;
+      0,                    0,  1,];
+end
